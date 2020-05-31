e4c3d9ebd17977f850a2481c0a07e78124b61956
bennnou <benjamin.longearet@elium.io>
2020-06-01 00:51:10 +0200

    Add Weather class

---
+++ src/app/shared/model/weather.ts
@@ -0,0 +1,34 @@
+import { IOpenWeatherMapOneCallResponse, IOpenWeatherMapOneCallWeatherResponse } from './openweathermap'
+import * as moment from 'moment'
+
+export class Weather {
+  public rawResponse: IOpenWeatherMapOneCallResponse
+
+  constructor(data: IOpenWeatherMapOneCallResponse) {
+    this.rawResponse = data
+  }
+
+  public getCurrentTime(): moment.Moment {
+    return moment(this.rawResponse.current.dt)
+  }
+
+  public getCurrentWeather(): IOpenWeatherMapOneCallWeatherResponse {
+    return this.rawResponse.current.weather[0]
+  }
+
+  public getCurrentTemperature(): number {
+    return this.rawResponse.current.temp
+  }
+
+  public getCurrentClouds(): number {
+    return this.rawResponse.current.clouds
+  }
+
+  public getCurrentHumidity(): number {
+    return this.rawResponse.current.humidity
+  }
+
+  public getCurrentWind(): number {
+    return this.rawResponse.current.wind_speed
+  }
+}
