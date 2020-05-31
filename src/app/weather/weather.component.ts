59bef8854573b165785e0d19cbcf40d3256f0973
bennnou <benjamin.longearet@elium.io>
2020-06-01 01:23:45 +0200

    Add basic information from Services

--- src/app/weather/weather.component.ts
+++ src/app/weather/weather.component.ts
@@ -1,4 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs'
+import { map } from 'rxjs/operators'
+import { Weather } from '../shared/model/weather'
+import { WeatherService } from '../shared/model/weather.service'
+
+export interface IWeatherComponentTitle {
+  date: string
+  weather: string
+}
+
+export interface IWeatherComponentTemperatures {
+  weatherIconUrl: string
+  temperature: number
+}
+
+export interface IWeatherComponentStats {
+  clouds: number
+  humidity: number
+  wind: number
+}
 
 @Component({
   selector: 'app-weather',
@@ -7,7 +27,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WeatherComponent implements OnInit {
 
-  constructor() { }
+  public weather$: Observable<Weather>
+  public weatherTitle$: Observable<IWeatherComponentTitle>
+  public weatherTemperatures$: Observable<IWeatherComponentTemperatures>
+  public weatherStats$: Observable<IWeatherComponentStats>
+
+  constructor(private weather: WeatherService) {
+    this.weather$ = this.weather.fetchAndGetWeather$(43.7190656,4.3057152)
+
+    this.weatherTitle$ = this.weather$.pipe(
+      map(weather => ({
+        date: `${weather.getCurrentTime().format('dddd HH')}:00`,
+        weather: weather.getCurrentWeather().description
+      }))
+    )
+
+    this.weatherTemperatures$ = this.weather$.pipe(
+      map(weather => ({
+        weatherIconUrl: `http://openweathermap.org/img/wn/${weather.getCurrentWeather().icon}@2x.png`,
+        temperature: weather.getCurrentTemperature()
+      }))
+    )
+
+    this.weatherStats$ = this.weather$.pipe(
+      map(weather => ({
+        clouds: weather.getCurrentClouds(),
+        humidity: weather.getCurrentHumidity(),
+        wind: weather.getCurrentWind()
+      }))
+    )
+  }
 
   ngOnInit(): void {
   }
