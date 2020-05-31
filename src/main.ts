95e35f418921438c82acd05d5d1760d3e50ab645
bennnou <benjamin.longearet@elium.io>
2020-06-01 01:23:27 +0200

    Add moment local for fr

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
+import * as moment from 'moment';
+moment.locale('fr')
+
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
